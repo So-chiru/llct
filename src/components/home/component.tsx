bedc155fb6f18ae01f7dd2979314a6d4480fbd3c
So-chiru <unleash9224@gmail.com>
2021-03-31 10:33:12 +0900

    Add react-transition-group, Tab animation
    This commit implements tab appear animation. that's all.

--- src/components/home/component.tsx
+++ src/components/home/component.tsx
@@ -1,9 +1,12 @@
 import DashboardTab from '@/tabs/dashboard'
 
-import React, { useRef } from 'react'
+import { CSSTransition } from 'react-transition-group'
+
+import React, { useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import '@/styles/components/home/home.scss'
+import '@/styles/animate.scss'
 
 const Pages: Record<string, JSX.Element> = {
   dashboard: <DashboardTab></DashboardTab>
@@ -36,41 +39,82 @@ const TabListComponent = ({ currentTab, tabs }: TabListProps) => {
   }
 
   return (
-    <div className='llct-tab-list'>
-      {tabs.map((tab, idx) => (
-        <div
-          className='llct-tab-button'
-          key={idx}
-          title={tab.name}
-          data-current={currentTab === idx}
-          onClick={() => tabUpdateHandler(idx)}
-        >
-          {tab.name}
-        </div>
-      ))}
-    </div>
+    <CSSTransition
+      in
+      appear={true}
+      addEndListener={(node, done) => {
+        node.addEventListener(
+          'transitionend',
+          () => {
+            done()
+          },
+          false
+        )
+      }}
+      classNames='llct-animate'
+    >
+      <div className='llct-tab-list'>
+        {tabs.map((tab, idx) => (
+          <div
+            className='llct-tab-button'
+            key={idx}
+            title={tab.name}
+            data-current={currentTab === idx}
+            onClick={() => tabUpdateHandler(idx)}
+          >
+            {tab.name}
+          </div>
+        ))}
+      </div>
+    </CSSTransition>
   )
+}
+
+const addClassOnLoad = (element: HTMLElement, style: string) => {
+  if (!element.classList.contains(style)) {
+    requestAnimationFrame(() => {
+      if (!element) {
+        return
+      }
+
+      element.classList.add(style)
+    })
+  }
 }
 
 const HomeComponent = ({ tabs, currentTab }: TabListProps) => {
   const icon = useRef<HTMLImageElement>(null)
 
-  if (icon.current && !icon.current.classList.contains('enter')) {
-    requestAnimationFrame(() => {
-      if (!icon.current) {
-        return
-      }
-
-      icon.current.classList.add('enter')
-    })
+  if (icon.current) {
+    addClassOnLoad(icon.current, 'enter')
   }
+
+  const [firstAnimate, setFirstAnimate] = useState(true)
 
   return (
     <div className='llct-app'>
       <img ref={icon} className='llct-icon' src='/images/logo/Icon.svg'></img>
       {tabs.map((tab, idx) => {
         if (idx === currentTab) {
-          return <TabComponent key={idx} tab={tab}></TabComponent>
+          return (
+            <CSSTransition
+              in
+              appear={true}
+              addEndListener={(node, done) => {
+                node.addEventListener(
+                  'transitionend',
+                  () => {
+                    setFirstAnimate(false)
+                    done()
+                  },
+                  false
+                )
+              }}
+              classNames={`llct-tab-animate${firstAnimate ? '' : '-nodelay'}`}
+            >
+              <TabComponent key={idx} tab={tab}></TabComponent>
+            </CSSTransition>
+          )
         }
       })}
       <TabListComponent currentTab={currentTab} tabs={tabs}></TabListComponent>
