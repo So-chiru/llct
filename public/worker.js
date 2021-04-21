8c4de67eb168a8f41b93de2b4a65a157d0560231
So-chiru <unleash9224@gmail.com>
2021-04-22 07:07:08 +0900

    Add self-destroying worker

--- public/worker.js
+++ public/worker.js
@@ -1,1 +1,14 @@
-const testWebWorker = ''
+self.addEventListener('install', function (e) {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll()
+    })
+    .then(function (clients) {
+      clients.forEach(client => client.navigate(client.url))
+    })
+})
