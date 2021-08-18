70c0d03684ea21784c2e9eda80e1134d209b1354
So-chiru <unleash9224@gmail.com>
2021-08-18 22:19:56 +0900

    Add playlist export feature

--- src/components/playlists/playlist-card/component.tsx
+++ src/components/playlists/playlist-card/component.tsx
@@ -1,5 +1,6 @@
 import MusicCardContainer from '@/components/music-card/container'
 import '@/styles/components/playlists/playlist-card.scss'
+import playlistUtils from '@/utils/playlists'
 import { concatClass } from '@/utils/react'
 import { songsDuration } from '@/utils/songs'
 
@@ -66,6 +67,35 @@ const DeleteIcon = (
     <path d='M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm-8 5v6h2v-6H9zm4 0v6h2v-6h-2zM9 4v2h6V4H9z' />
   </svg>
 )
+
+const ExportIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    width='24'
+    height='24'
+  >
+    <path fill='none' d='M0 0h24v24H0z' />
+    <path d='M3 19h18v2H3v-2zm10-5.828L19.071 7.1l1.414 1.414L12 17 3.515 8.515 4.929 7.1 11 13.17V2h2v11.172z' />
+  </svg>
+)
+
+const printExport = (item: MusicPlaylist) => {
+  const str = playlistUtils.exportPlaylist(playlistUtils.idify(item))
+
+  if ('share' in navigator) {
+    navigator.share({
+      text: str
+    })
+
+    return
+  }
+
+  prompt(
+    '아래 문자를 복사하고 다른 기기에서 플레이리스트 불러오기 버튼을 눌러 플레이리스트를 복사할 수 있습니다.',
+    str
+  )
+}
 
 export const PlaylistCardComponent = ({
   item,
@@ -137,6 +167,9 @@ export const PlaylistCardComponent = ({
             onClick={() => onDeleteClick && onDeleteClick()}
           >
             <span>{DeleteIcon}</span>
+          </div>
+          <div className='section button' onClick={() => printExport(item)}>
+            <span>{ExportIcon}</span>
           </div>
           {folded && (
             <div className='section musics'>
