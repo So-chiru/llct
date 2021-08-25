e89138156d8df723228c58e253e45d8aa7fb0747
So-chiru <unleash9224@gmail.com>
2021-08-25 20:37:26 +0900

    Fix dashboard to have 4 cards

--- src/tabs/dashboard.tsx
+++ src/tabs/dashboard.tsx
@@ -17,16 +17,24 @@ const DashboardTab = ({ show }: LLCTTabProps) => {
       {data && data.error && (
         <EmptyComponent text={`${data.error}`} height='30vh'></EmptyComponent>
       )}
-      <div className='card-lists'>
-        {!data || !data.items
-          ? [...new Array(12)].map((v, i) => {
-              return (
-                <MusicCard key={`template:${i}`} skeleton={true}></MusicCard>
-              )
-            })
-          : randomSongs(data.items, 12).map((v, i) => {
-              return <MusicCard key={`template:${i}`} id={v}></MusicCard>
-            })}
+      <div className='dashboard-tab'>
+        <div className='dashboard-section'>
+          <h3 className='dashboard-section-title'>랜덤 곡을 뽑아볼까요?</h3>
+          <div className='card-lists'>
+            {!data || !data.items
+              ? [...new Array(4)].map((v, i) => {
+                  return (
+                    <MusicCard
+                      key={`template:${i}`}
+                      skeleton={true}
+                    ></MusicCard>
+                  )
+                })
+              : randomSongs(data.items, 4).map((v, i) => {
+                  return <MusicCard key={`template:${i}`} id={v}></MusicCard>
+                })}
+          </div>
+        </div>
       </div>
     </div>
   )
