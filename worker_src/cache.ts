fa76ce7d941ca3edb05ac5a73f5132b911145a0a
So-chiru <unleash9224@gmail.com>
2021-09-10 15:26:07 +0900

    Bump service worker version

--- worker_src/cache.ts
+++ worker_src/cache.ts
@@ -1,6 +1,6 @@
 const sw = (self as unknown) as ServiceWorkerGlobalScope
 
-export const STATIC_CACHE = '@llct/cache/static/v18'
+export const STATIC_CACHE = '@llct/cache/static/v19'
 export const DYNAMIC_CACHE = '@llct/cache/dynamic/v1'
 
 type CacheStorageKey = typeof STATIC_CACHE | typeof DYNAMIC_CACHE
